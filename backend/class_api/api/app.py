cbab47ff57dec3472036894c5f23158c88b8ea9b
mikosa01 <michaelmerry8@gmail.com>
2025-03-06 10:27:37 +0000

    new update

--- backend/class_api/api/app.py
+++ backend/class_api/api/app.py
@@ -12,8 +12,9 @@ def create_app(config_name='development'):
 
     return app
 
-app = create_app(config_name='development')
+
 
 if __name__ == '__main__':
+    app = create_app(config_name='development')
     port =int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
